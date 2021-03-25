0c67ff4ed13dc0b4c966d4441b98d912f2d9db5e
Alvaro Senorale <alvaro@xmartlabs.com>
2021-03-25 13:35:57 -0300

    setup sqlalchemy and user model

---
+++ alembicdb/versions/29b594989ff3_create_users_table.py
@@ -0,0 +1,29 @@
+"""create users table
+
+Revision ID: 29b594989ff3
+Revises: 
+Create Date: 2021-03-23 11:59:27.351613
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '29b594989ff3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+  op.create_table(
+    'users',
+    sa.Column('id', sa.Integer, primary_key=True),
+    sa.Column('name', sa.String(50), nullable=False),
+    sa.Column('email', sa.String(100), nullable=False)
+  )
+
+
+def downgrade():
+  op.drop_table('users')
---
+++ alembicdb/versions/e1ebfb41523d_add_password_to_users.py
@@ -0,0 +1,24 @@
+"""Add password to users
+
+Revision ID: e1ebfb41523d
+Revises: 29b594989ff3
+Create Date: 2021-03-23 12:27:09.492275
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e1ebfb41523d'
+down_revision = '29b594989ff3'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+  op.add_column('users', sa.Column('password_hash', sa.String(100)))
+
+
+def downgrade():
+  op.drop_column('users', 'password_hash')
