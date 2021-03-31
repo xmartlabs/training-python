05b17940badf0b46baf6dc8f1e1d2eabce36687d
Alvaro Senorale <alvaro@xmartlabs.com>
2021-03-31 11:13:54 -0300

    Create require_login view decorator

--- blueprints/users/views.py
+++ blueprints/users/views.py
@@ -1,29 +1,50 @@
-from flask import Blueprint, request, current_app, jsonify
+from flask import Blueprint, request, current_app, jsonify, g
 import jwt
 from sqlalchemy import select
 from models.user import User
 from database import db
 import bcrypt
+from functools import wraps
 
 users = Blueprint('users', __name__)
+ 
+def require_login(func):
+  @wraps(func)
+
+  def inner(*args, **kwargs):
+    if g.current_user is None:
+      return 'unauthorized', 401
+
+    return func()
+
+  return inner
+
 
 def jwt_secret():
   return current_app.config['JWT_SECRET']
 
-@users.route('/me', methods=['GET'])
-def me():  
-  token = request.headers['Authorization']
+@users.before_request
+def set_current_user():
+  token = request.headers.get('Authorization', None)
+
+  if token is None:
+    return
+
   payload = jwt.decode(token, jwt_secret(), algorithms=["HS256"])
   
   stmt = select(User).where(User.email == payload['user_email'])
-  user = db.session.execute(stmt).fetchone()
-   
-  if (user == None):
-    return jsonify('unauthorized'), 401
-  else:
-    user = user[0]
+  user = db.session.execute(stmt).fetchone() 
   
-  return jsonify({'id': user.id, 'name': user.name})
+  if user is not None:
+    g.current_user = user[0]
+  else:
+    g.current_user = None
+  
+
+@users.route('/me', methods=['GET'])
+@require_login
+def me():  
+  return jsonify({'id': g.current_user.id, 'name': g.current_user.name})
 
 @users.route('/login', methods=['POST'])
 def login():  
@@ -44,6 +65,7 @@ def login():
     return jsonify('wrong credentials'), 401
 
 @users.route('/users', methods=['GET'])
+@require_login
 def list_users():  
   user_response = []
 
--- database.py
+++ database.py
@@ -3,5 +3,4 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def setup(app):
-  print(app.config)
   db.init_app(app)
